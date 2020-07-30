e3b86a0f6ff42a8abe3cd16f7bfcd002d2b38094
Niraj Patel <786nirajpatel@gmail.com>
2020-07-30 00:52:27 -0400

    Added new files
    -Changed .sh to .bat
    -Made automation.py
    -Made .gitignore for all .env files

---
+++ automation.py
@@ -0,0 +1,5 @@
+import sys
+import os
+from github import Github
+
+print("HELLO")
--- create.bat
+++ create.bat
@@ -1,3 +1,5 @@
 @echo off
+python N:\Personal\Code\Project\Automation-for-Github-Repos\automation.py
 echo This is a script that does nothing!
+dir
 pause

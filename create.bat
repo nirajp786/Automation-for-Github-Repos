09abddf7602bb400205156e691d2b1e775d21fd1
Niraj Patel <786nirajpatel@gmail.com>
2020-07-30 01:58:30 -0400

    Changed create.bat
    -Set up envrionment variables for drive, and project folder
    -cd into project folder

--- create.bat
+++ create.bat
@@ -1,5 +1,10 @@
 @echo off
 python N:\Personal\Code\Project\Automation-for-Github-Repos\automation.py
 echo This is a script that does nothing!
-dir
+echo %1
+echo %GithubUsername%
+echo %ProjectDrive%
+echo %ProjectLocation%
+%ProjectDrive%
+cd %ProjectLocation%
 pause
